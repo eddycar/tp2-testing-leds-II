99f9a96b3e6cc5cccf2c93972ad0abeb239a33f3
eddycar <edicarvajalp@gmail.com>
2024-10-11 01:44:20 -0500

    Se añaden comentarios

--- inc/leds.h
+++ inc/leds.h
@@ -1,3 +1,11 @@
+/**
+ * @file leds.h
+ * @author Edilberto Carvajal
+ * @date 11/10/2024
+ *
+ * @brief Archivo de cabecera biblioteca leds
+ */
+
 #ifndef LEDS_H
 #define LEDS_H
 
@@ -7,18 +15,52 @@
 #define ALL_LED_OFF 0x0000
 #define ALL_LED_ON  0xFFFF
 
+/**
+ * @brief Inicializa el puerto y apaga todos los LEDs
+ *
+ * @param direccion
+ * @return true
+ * @return false
+ */
 bool CrearLeds(uint16_t * direccion);
 
+/**
+ * @brief Enciende un LED
+ *
+ * @param led
+ * @return true
+ * @return false
+ */
 bool EncenderLeds(int led);
 
+/**
+ * @brief apaga un LED
+ *
+ * @param led
+ * @return true
+ * @return false
+ */
 bool ApagarLeds(int led);
 
+/**
+ * @brief Enciende todos los LEDs
+ *
+ */
 void EncenderTodosLosLeds(void);
 
+/**
+ * @brief Apaga todos los LEDs
+ *
+ */
 void ApagarTodosLosLeds(void);
 
+/**
+ * @brief Consulta si un LED está prendido
+ *
+ * @param led
+ * @return true
+ * @return false
+ */
 bool EstaElLedEncendido(int led);
-
-bool EstaElLedApagado(int led);
 
 #endif // LEDS_H
--- src/leds.c
+++ src/leds.c
@@ -1,9 +1,21 @@
+/**
+ * @file leds.h
+ * @author Edilberto Carvajal
+ * @date 11/10/2024
+ *
+ * @brief Archivo de implementación de biblioteca leds
+ */
+
 #include "leds.h"
 #include "stddef.h"
 
 #define LBS         0x0001
 #define LED_OFFSET  1
 
+/**
+ * @brief Si está en modo DEBUG hace los chequeos
+ *
+ */
 #ifdef DEBUG
 #define ASSERT(arg)
 if (arg) {
@@ -19,14 +31,34 @@ if (arg) {
 
 static uint16_t * puerto;
 
+/**
+ * @brief Comprueba que el valor de led este dentro de los limites
+ *
+ * @param led
+ * @return true
+ * @return false
+ */
 static bool ComprobarLimites(int led) {
     return ((FIRST_LED <= led) && (LAST_LED >= led));
 }
 
+/**
+ * @brief Mascara de bits para led
+ *
+ * @param led
+ * @return uint16_t
+ */
 uint16_t LedToMask(int led) {
     return LBS << (led - LED_OFFSET);
 }
 
+/**
+ * @brief Inicializa el puerto y apaga todos los LEDs
+ *
+ * @param direccion
+ * @return true
+ * @return false
+ */
 bool CrearLeds(uint16_t * direccion) {
     if (NULL != direccion) {
         puerto = direccion;
@@ -37,6 +69,13 @@ bool CrearLeds(uint16_t * direccion) {
     }
 }
 
+/**
+ * @brief Enciende un LED
+ *
+ * @param led
+ * @return true
+ * @return false
+ */
 bool EncenderLeds(int led) {
     if (false != ComprobarLimites(led)) {
         *puerto |= LedToMask(led);
@@ -46,6 +85,13 @@ bool EncenderLeds(int led) {
     }
 }
 
+/**
+ * @brief apaga un LED
+ *
+ * @param led
+ * @return true
+ * @return false
+ */
 bool ApagarLeds(int led) {
     if (false != ComprobarLimites(led)) {
         *puerto &= ~LedToMask(led);
@@ -55,16 +101,31 @@ bool ApagarLeds(int led) {
     }
 }
 
+/**
+ * @brief Enciende todos los LEDs
+ *
+ */
 void EncenderTodosLosLeds(void) {
     ASSERT(NULL != Puerto);
     *puerto = ALL_LED_ON;
 }
 
+/**
+ * @brief Apaga todos los LEDs
+ *
+ */
 void ApagarTodosLosLeds(void) {
     ASSERT(NULL != Puerto);
     *puerto = ALL_LED_OFF;
 }
 
+/**
+ * @brief Consulta si un LED está prendido
+ *
+ * @param led
+ * @return true
+ * @return false
+ */
 bool EstaElLedEncendido(int led){
     ASSERT(NULL != puerto);
     return (*puerto & LedToMask(led)) != 0; 
